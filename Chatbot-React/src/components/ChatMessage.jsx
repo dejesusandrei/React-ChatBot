0c8a771aa727a39fa21ee50e5d06ab6a6bab90df
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-07-30 21:33:28 +0800

    Responsive

--- Chatbot-React/src/components/ChatMessage.jsx
+++ Chatbot-React/src/components/ChatMessage.jsx
@@ -6,11 +6,11 @@ import ReactMarkdown from 'react-markdown';
 export function ChatMessage({message, sender}){
   return(
     <div className={`flex ${sender === 'user' ? 'justify-end' : 'justify-start'} items-start gap-x-3  mb-6`}>
-      {sender === 'chatdong' && <img src={chatdong} className="cursor-pointer w-12.5" alt="Chatdong Icon" />} 
+      {sender === 'chatdong' && <img src={chatdong} className="cursor-pointer w-10.5 lg:w-12.5" alt="Chatdong Icon" />} 
       <div className="text-[clamp(14px,3vw,17px)] bg-[rgb(238,238,238)] max-w-100 font-medium text-black leading-relaxed px-5 py-3.75 markdown-content rounded-[10px]">
         {typeof message === 'string' ? (<ReactMarkdown>{message}</ReactMarkdown>) : (message)}
       </div>
-      {sender === 'user' && <img src={user} className="cursor-pointer w-12.5" alt="User Icon" />} 
+      {sender === 'user' && <img src={user} className="cursor-pointer w-10.5 lg:w-12.5" alt="User Icon" />} 
     </div>
   );
 }
